53dda64fb7fae6608b2e4921ad56f6653adb131b
whaleism <230928187+whaleism@users.noreply.github.com>
2026-01-09 10:03:07 -0600

    Added links to footer and fixed white screen of death by adding root back.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        Project coded by{" "}
+        <a href="https://github.com/whaleism" target="_blank" rel="noreferrer">
+          Jenny Lee
+        </a>
+        , opened sourced on{" "}
+        <a
+          href="https://github.com/whaleism/react-weather-app-scw5"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+        , and hosted by{" "}
+        <a
+          href="https://glittery-llama-df5134.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
