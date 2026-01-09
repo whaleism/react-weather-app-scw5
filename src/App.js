e7bcb936752d817059e3474576ca4b3a8e49d0e9
whaleism <230928187+whaleism@users.noreply.github.com>
2026-01-09 10:15:38 -0600

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,29 +3,35 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        Project coded by{" "}
-        <a href="https://github.com/whaleism" target="_blank" rel="noreferrer">
-          Jenny Lee
-        </a>
-        , opened sourced on{" "}
-        <a
-          href="https://github.com/whaleism/react-weather-app-scw5"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-        , and hosted by{" "}
-        <a
-          href="https://glittery-llama-df5134.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Netlify
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          Project coded by{" "}
+          <a
+            href="https://github.com/whaleism"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Jenny Lee
+          </a>
+          , opened sourced on{" "}
+          <a
+            href="https://github.com/whaleism/react-weather-app-scw5"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+          , and hosted by{" "}
+          <a
+            href="https://glittery-llama-df5134.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
