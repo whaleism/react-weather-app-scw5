ce8d669efac188576d340775e8041267d08d6f23
whaleism <230928187+whaleism@users.noreply.github.com>
2026-01-12 14:09:05 -0600

    Display forecast of 1 day without calling api yet

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -17,7 +17,7 @@ export default function WeatherInfo(props) {
         <div className="col-6">
           <div className="clearfix">
             <div className="float-start">
-              <WeatherIcon code={props.data.icon} />
+              <WeatherIcon code={props.data.icon} size={50} />
             </div>
 
             <div className="float-start">
